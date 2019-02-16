bbaaf166f1a3e72fa98cbea68a257552be52954a
Lorenzo Milesi <lmilesi@netemedia.fr>
2019-02-16 20:13:42 +0100

    Model tests

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -8,9 +8,13 @@
   <body>
     <h1>Birdboard</h1>
     <ul>
-      @foreach ($projects as $project)
-        <li>{{ $project->title }}</li>
-      @endforeach
+      @forelse ($projects as $project)
+        <li>
+          <a href="{{ $project->path() }}">{{ $project->title }}</a>
+        </li>
+      @empty
+        <li>{{ __('There are no projects yet.') }}</li>
+      @endforelse
     </ul>
   </body>
 </html>
